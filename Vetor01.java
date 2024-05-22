a0b03e53a57d3f5708ae7b7f8c952c673588378e
DudaSantoni <159277791+DudaSantoni@users.noreply.github.com>
2024-05-22 16:12:22 -0300

    Vetores

---
+++ Vetor01.java
@@ -0,0 +1,23 @@
+package vetores;
+
+import java.util.Scanner;
+
+public class Vetor01 {
+
+	public static void main(String[] args) {
+		String a[] = new String [5];
+
+		Scanner ler = new Scanner(System.in);
+
+		for (int i =0; i<5; i++) {
+			System.out.println("Informe um valor " + i + "..:");
+			a [i]= ler.nextLine();
+		}
+		for (int i = 0; i<5; i++) {
+			System.out.println(a[i]);
+		}
+		ler.close();
+
+	}
+
+}
